2ee3886a85482155dca8ece0ef9fe09e34f38366
Alp Beyazgül <alpbeyazgul@hotmail.com>
2024-02-28 03:31:52 +0300

    Create Run.js
    Visit http://localhost:3000 in your web browser, and you should be able to use the real-time chat application.

---
+++ Run.js
@@ -0,0 +1,6 @@
+# Run the Application:
+# In the terminal, run the following command:
+
+node server.js
+
+## Visit http://localhost:3000 in your web browser, and you should be able to use the real-time chat application.
